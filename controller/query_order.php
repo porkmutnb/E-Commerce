c6059441810e158a38481e55348c7b4922e378c6
porkmutnb12 <kmutnbenetpor@gmail.com>
2019-03-04 23:17:34 +0700

    no message

--- controller/query_order.php
+++ controller/query_order.php
@@ -4,7 +4,7 @@
     }
     include('./controller/connectDB.php');
 
-    $sql = "SELECT tbl_order.orderID, tbl_order.address, tbl_order.evidence, tbl_order.status, tbl_order.created_at, tbl_product.productName, tbl_product.price FROM tbl_order JOIN tbl_order_detail ON tbl_order_detail.orderID = tbl_order.orderID JOIN tbl_product ON tbl_product.productID = tbl_order_detail.productID WHERE `userID` = '".$user['userID']."' AND `status` != 0";
+    $sql = "SELECT tbl_order.orderID, tbl_order.address, tbl_order.evidence, tbl_order.status, tbl_order.created_at FROM tbl_order WHERE `userID` = '".$user['userID']."' AND `status` != 0";
 
     $order = mysqli_query($conn, $sql);
 
